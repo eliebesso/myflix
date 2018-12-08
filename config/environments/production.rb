f8badfd7d3b4391d229eaf0120b035750d80be98
elie@meachei.com.br <elie@meachei.com.br>
2018-12-08 17:09:31 -0200

    Add domain to action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Myflix::Application.configure do
     :enable_starttls_auto => true
   }
 
-  config.action_mailer.default_url_options = { protocol: 'https', :host => 'www.meachei.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', :host => 'www.quiet-castle-72726.herokuapp.com' }
 
 end
 
