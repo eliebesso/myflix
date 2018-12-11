584a976a3c42f137187aea35db2c9ceec716f372
elie@meachei.com.br <elie@meachei.com.br>
2018-12-11 13:49:39 -0200

    4th sidekiq fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,7 +10,6 @@ if Rails.env.production?
     config.redis = { url: ENV[ENV['REDIS_URL']] }
     
     Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       ActiveRecord::Base.connection_pool.disconnect!
 
       ActiveSupport.on_load(:active_record) do
@@ -19,8 +18,6 @@ if Rails.env.production?
         # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
         config['pool'] = 16
         ActiveRecord::Base.establish_connection(config)
-
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       end
     end
   end
